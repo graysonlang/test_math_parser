4451ca06342b9da1caf198aa58a05da0da2a8e36
Grayson Lang <graysonlang@gmail.com>
2017-03-16 19:05:50 -0700

    Fixing up function signatures.

--- src/MathParser.h
+++ src/MathParser.h
@@ -39,8 +39,8 @@ namespace MathParser {
     size_t error_length;
 
     Result(double result);
-    Result(ParsingErrorType, std::string && = "", size_t = 0, size_t = 0);
-    Result(EvaluationErrorType, std::string && = "", size_t = 0, size_t = 0);
+    Result(ParsingErrorType, std::string &&filtered_expression = "", size_t error_position = 0, size_t error_length = 0);
+    Result(EvaluationErrorType, std::string &&filtered_expression = "", size_t error_position = 0, size_t error_length = 0);
   };
 
   Result evaluate_expression(const std::string &expression, double current_value = std::numeric_limits<double>::quiet_NaN());
